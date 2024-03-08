526a1ef4bd9c802920f9f98e80343f27978e3592
RonanCraft <SuperRonanCraft@gmail.com>
2024-03-08 11:20:52 -0500

    light-dark mode updates class

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,28 @@
 const toggleButton = document.getElementById("mode-toggle");
 const content = document.querySelector("body");
 
+let mode = "light";
+
+// Listen for a click event on toggle switch
 toggleButton.addEventListener("click", function () {
-  const isDarkMode = content.classList.toggle("dark-mode");
-  if (isDarkMode) {
-    darkMode();
-  } else {
+  // If mode is dark, apply light background
+  if (mode === "dark") {
+    mode = "light";
     lightMode();
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = "dark";
+    darkMode();
   }
 });
 
 function lightMode() {
   toggleButton.firstElementChild.setAttribute("src", `assets/images/moon.png`);
+  content.setAttribute("class", "light");
 }
 
 function darkMode() {
   toggleButton.firstElementChild.setAttribute("src", `assets/images/sun.png`);
+  content.setAttribute("class", "dark");
 }
