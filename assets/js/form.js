6e31a69a46d120bdf90f6f90500e2d8d4ed792a2
RonanCraft <SuperRonanCraft@gmail.com>
2024-03-09 23:37:19 -0500

    required default form behavior

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,9 +3,15 @@ const blogAuthorInput = document.getElementById("author");
 const blogTitleInput = document.getElementById("title");
 const blogContentInput = document.getElementById("content");
 const submitButton = document.getElementById("submit-button");
+const formEl = document.getElementById("form");
 
 // Click event for submit button
 function newSubmission(event) {
+  // Does any input field not have a value?
+  if (!formEl.checkValidity()) {
+    alert("Please fill out all fields.");
+    return;
+  }
   // Stop the form button from reloading the page
   event.preventDefault();
 
@@ -16,13 +22,12 @@ function newSubmission(event) {
     content: blogContentInput.value,
   };
 
-  // Does any input field not have a value?
-  if (!blogPost.author || !blogPost.title || !blogPost.content) {
-    console.log("No Content");
-    document.getElementById("error").style.display = "block";
-    alert("Please fill out all fields.");
-    return;
-  }
+  // if (!blogPost.author || !blogPost.title || !blogPost.content) {
+  //   console.log("No Content");
+  //   document.getElementById("error").style.display = "block";
+  //   //alert("Please fill out all fields.");
+  //   return;
+  // }
 
   // Get existing posts or initialize empty array
   var existingPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
